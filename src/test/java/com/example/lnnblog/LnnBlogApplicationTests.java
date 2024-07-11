9698d0b6aa8f85db2e9c2ab02e8c01fbadbae1fb
lnn <3238773417@qq.com>
2024-07-12 01:17:01 +0800

    1

---
+++ src/test/java/com/example/lnnblog/LnnBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lnnblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LnnBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

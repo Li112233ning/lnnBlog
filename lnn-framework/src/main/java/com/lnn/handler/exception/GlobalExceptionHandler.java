e540b3db5973cd77bc3ca5b69fd8069063a369b3
lnn <3238773417@qq.com>
2024-07-26 19:00:47 +0800

    认证授权失败处理&&统一异常处理

---
+++ lnn-framework/src/main/java/com/lnn/handler/exception/GlobalExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.lnn.handler.exception;
+
+import com.lnn.domin.ResponseResult;
+import com.lnn.enums.AppHttpCodeEnum;
+import com.lnn.exception.SystemException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ControllerAdvice
+@ResponseBody
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(SystemException.class)
+    public ResponseResult systemExceptionHandler(SystemException e){
+
+        log.error("出现了异常！ {}",e);
+        //从异常对象中获取提示信息封装返回
+        return ResponseResult.errorResult(e.getCode(),e.getMsg());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseResult exceptionHandler(Exception e){
+        //打印异常信息
+        log.error("出现了异常！ {}",e);
+        //从异常对象中获取提示信息封装返回
+        return ResponseResult.errorResult(AppHttpCodeEnum.SYSTEM_ERROR.getCode(),e.getMessage());
+    }
+
+}

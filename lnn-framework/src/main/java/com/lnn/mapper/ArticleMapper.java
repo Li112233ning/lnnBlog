2a03f84ba4dfe8ca0a4e12601ed44a744cc62f51
Li112233ning <81576772+Li112233ning@users.noreply.github.com>
2024-07-30 16:47:31 +0800

    Delete lnn-framework directory

--- lnn-framework/src/main/java/com/lnn/mapper/ArticleMapper.java
+++
@@ -1,9 +0,0 @@
-package com.lnn.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.lnn.domin.entity.Article;
-
-public interface ArticleMapper extends BaseMapper<Article> {
-
-
-}
--- lnn-framework/src/main/java/com/lnn/service/UploadService.java
+++
@@ -1,9 +0,0 @@
-package com.lnn.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.lnn.domin.ResponseResult;
-import org.springframework.web.multipart.MultipartFile;
-
-public interface UploadService {
-    ResponseResult uploadImg(MultipartFile img);
-}

3ef00d034dee8bac1e2c814ae1d8f0a05832b3ac
lnn <3238773417@qq.com>
2024-07-30 14:44:38 +0800

    后台权限控制及动态路由

--- lnn-framework/src/main/java/com/lnn/service/impl/SystemLoginServiceImpl.java
+++ lnn-framework/src/main/java/com/lnn/service/impl/SystemLoginServiceImpl.java
@@ -6,6 +6,7 @@ import com.lnn.domin.entity.User;
 import com.lnn.service.LoginService;
 import com.lnn.utils.JwtUtil;
 import com.lnn.utils.RedisCache;
+import com.lnn.utils.SecurityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -41,5 +42,14 @@ public class SystemLoginServiceImpl implements LoginService {
         Map<String, String> map = new HashMap<>();
         map.put("token", jwt);
         return ResponseResult.okResult(map);
+    }
+
+    @Override
+    public ResponseResult logout() {
+        //获取当前登录的用户id
+        Long userId = SecurityUtils.getUserId();
+        //删除redis中对应的值
+        redisCache.deleteObject("login:"+userId);
+        return ResponseResult.okResult();
     }
 }
